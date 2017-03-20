8b67c0551a43feceebf3575d43aeb635885f36d7
Stephanie Booth <booth.sl@gmail.com>
2017-03-20 19:27:53 -0400

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+(function(){
+  d3.select("body").append("image")
+    .attr('x',0)
+    .attr('y',0)
+    .attr('width', 200)
+    .attr('height', 240)
+    .attr("xlink:href","../img/6040_2_2.tif")
+})();
+//d3.select("svg").append("image")
+//  .attr("xlink:href","../img/6040_2_2.tif");
