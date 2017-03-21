dded3b468daf84cdac54ce55eb55490a5070b683
Stephanie Booth <booth.sl@gmail.com>
2017-03-20 20:49:04 -0400

    Changed tif to png, got img to render.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-(function(){
-  d3.select("body").append("image")
-    .attr('x',0)
-    .attr('y',0)
+window.onload = function(){
+  d3.select("svg").append("image")
+    .attr('x', 0)
+    .attr('y', 0)
     .attr('width', 200)
     .attr('height', 240)
-    .attr("xlink:href","../img/6040_2_2.tif")
-})();
+    .attr("xlink:href","/home/stephanie/MyLibrary/dstl_satellite_viz/src/img/6040_2_2.png");
+};
+
 //d3.select("svg").append("image")
 //  .attr("xlink:href","../img/6040_2_2.tif");
