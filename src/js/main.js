d15d3e609a653df828c5bf95812b96d6e04f4f53
Stephanie Booth <booth.sl@gmail.com>
2017-03-25 18:47:10 -0400

    Got the image to render properly, starting drawing paths.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,32 @@
 window.onload = function(){
-  d3.select("svg").append("image")
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('width', 200)
-    .attr('height', 240)
-    .attr("xlink:href","/home/stephanie/MyLibrary/dstl_satellite_viz/src/img/6040_2_2.png");
+  d3.json('./data/002_TR_L6_FOOTPATH_TRAIL.geojson', function(error, data) {
+    let svg = d3.select('svg')
+      .attr('width', '921px')
+      .attr('height', '817px')
+      .attr('padding', '0px 0px 0px 0px');
+
+    svg.append('image')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('xlink:href','./img/6040_2_2.png');
+
+    svg.select('body')
+      .attr('margin', '0px 0px 0px 0px');
+
+    var path = d3.geoPath();
+    svg.selectAll('path')
+      .data(data.features)
+      .enter()
+      .append('path')
+        .attr('d', path)
+        .attr('fill', 'orange')
+        .attr('stroke', 'black')
+        .attr('stroke-width', '3');
+
+  });
 };
 
-//d3.select("svg").append("image")
-//  .attr("xlink:href","../img/6040_2_2.tif");
+//d3.select('svg').append('image')
+//  .attr('xlink:href','../img/6040_2_2.tif');
