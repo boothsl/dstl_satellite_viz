84f9087b4786d9e30bd41bbf9a45b772b24cb107
chris.f.hull@gmail.com <chris.f.hull@gmail.com>
2017-05-14 12:37:34 -0400

    adds style guide and applies to code

--- build/js/main.js
+++ build/js/main.js
@@ -7,15 +7,16 @@ $(document).ready(function () {
     paddingBottom: '4em'
   });
 
-  var svg = d3.selectAll('svg').attr('width', '100%').attr('height', '1000').attr('padding', '0px 0px 0px 0px');
+  d3.selectAll('svg').attr('width', '100%').attr('height', '1000').attr('padding', '0px 0px 0px 0px');
 
   var imagePaths = ['src/img/6040_2_2.png', 'src/img/6060_2_3.png', 'src/img/6070_2_3.png', 'src/img/6100_1_3.png'];
-  $.getJSON("src/data/folderStruct.json", function (data) {
+
+  $.getJSON('src/data/folderStruct.json', function (data) {
     data.forEach(function (folder, i) {
       var slide = d3.select('#s' + (i + 1) + ' .fp-tableCell');
 
-      var imagePath = $.grep(imagePaths, function (imagePath) {
-        return imagePath.indexOf(folder.foldername) !== -1;
+      var imagePath = $.grep(imagePaths, function (x) {
+        return x.indexOf(folder.foldername) !== -1;
       });
 
       var imgContainer = slide.append('div').attr('class', 'd3-img-container');
@@ -24,26 +25,25 @@ $(document).ready(function () {
 
       var imgElem = svg.append('image');
 
-      var img = new Image();
-
-      var toggler = slide.append('button').attr('class', 'overlayToggle').text("Toggle Overlay").on('click', function () {
-        slide.selectAll('.overlays').style("visibility", function () {
-          return d3.select(this).style("visibility") == "hidden" ? 'visible' : 'hidden';
+      slide.append('button').attr('class', 'overlayToggle').text('Toggle Overlay').on('click', function () {
+        slide.selectAll('.overlays').style('visibility', function () {
+          return d3.select(this).style('visibility') === 'hidden' ? 'visible' : 'hidden';
         });
       });
 
-      img.src = imagePath;
-      img.onload = function (img) {
-        //Images are defaulting to 0,0 on some browsers. So this is a workaround
+      var image = new Image();
+      image.src = imagePath;
+      image.onload = function (img) {
+        // Images are defaulting to 0,0 on some browsers.
         var width = img.srcElement.width;
         var height = img.srcElement.height;
         imgElem.attr('height', height * 0.7).attr('width', width * 0.7).attr('xlink:href', imagePath);
-        imgContainer.style('height', height * 0.7 + "px").style('width', width * 0.7 + "px").style('margin', "auto auto 10px auto");
+        imgContainer.style('height', height * 0.7 + 'px').style('width', width * 0.7 + 'px').style('margin', 'auto auto 10px auto');
       };
 
       folder.files.forEach(function (file) {
-        d3.json('src/data/' + folder.foldername + '/' + file.filename + '.geojson', function (error, data) {
-          data.features.forEach(function (feature) {
+        d3.json('src/data/' + folder.foldername + '/' + file.filename + '.geojson', function (geoJSONData) {
+          geoJSONData.features.forEach(function (feature) {
             feature.geometry.coordinates.forEach(function (coord) {
               coord.forEach(function (subCoord) {
                 subCoord[0] *= folder.xFactor * 0.7;
@@ -54,7 +54,7 @@ $(document).ready(function () {
           var overlays = svg.append('g').attr('class', 'overlays');
 
           var path = d3.geoPath();
-          overlays.selectAll('path').data(data.features).enter().append('path').attr('d', path).attr('fill', file.color).attr('opacity', 0.7).attr('stroke', 'black').attr('stroke-width', '1');
+          overlays.selectAll('path').data(geoJSONData.features).enter().append('path').attr('d', path).attr('fill', file.color).attr('opacity', 0.7).attr('stroke', 'black').attr('stroke-width', '1');
         });
       });
     });
--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,82 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $('#fullpage').fullpage({
-      sectionsColor: ['#008080','#111111','#006A00','#008080'],
-      verticalCentered: true,
-      paddingBottom: '4em'
+    sectionsColor: ['#008080', '#111111', '#006A00', '#008080'],
+    verticalCentered: true,
+    paddingBottom: '4em'
   });
 
-  let svg = d3.selectAll('svg')
-  .attr('width', '100%')
-  .attr('height', '1000')
-  .attr('padding', '0px 0px 0px 0px');
+  d3.selectAll('svg')
+    .attr('width', '100%')
+    .attr('height', '1000')
+    .attr('padding', '0px 0px 0px 0px');
 
   const imagePaths = [
     'src/img/6040_2_2.png',
     'src/img/6060_2_3.png',
     'src/img/6070_2_3.png',
     'src/img/6100_1_3.png'
-  ]
-  $.getJSON("src/data/folderStruct.json", (data) => {
+  ];
+
+  $.getJSON('src/data/folderStruct.json', (data) => {
     data.forEach((folder, i) => {
-    let slide = d3.select('#s'+(i+1) + ' .fp-tableCell');
+      const slide = d3.select(`#s${i + 1} .fp-tableCell`);
 
-    let imagePath = $.grep(imagePaths, imagePath => imagePath.indexOf(folder.foldername) !== -1);
+      const imagePath = $.grep(imagePaths, x => x.indexOf(folder.foldername) !== -1);
 
-    let imgContainer = slide.append('div')
-    .attr('class', 'd3-img-container');
+      const imgContainer = slide.append('div')
+        .attr('class', 'd3-img-container');
 
-    let svg = imgContainer.append('svg');
+      const svg = imgContainer.append('svg');
 
-    let imgElem = svg.append('image');
+      const imgElem = svg.append('image');
 
-    let img = new Image();
 
-    let toggler = slide.append('button')
-    .attr('class','overlayToggle')
-    .text("Toggle Overlay")
-    .on('click', () => {
-      slide.selectAll('.overlays')
-      .style("visibility", function() {
-        return d3.select(this).style("visibility") == "hidden" ? 'visible' : 'hidden';
-      });
+      slide.append('button')
+        .attr('class', 'overlayToggle')
+        .text('Toggle Overlay')
+        .on('click', () => {
+          slide.selectAll('.overlays')
+          .style('visibility', function () {
+            return d3.select(this).style('visibility') === 'hidden' ? 'visible' : 'hidden';
+          });
+        });
 
-    });
+      const image = new Image();
+      image.src = imagePath;
+      image.onload = (img) => { // Images are defaulting to 0,0 on some browsers.
+        const width = img.srcElement.width;
+        const height = img.srcElement.height;
+        imgElem.attr('height', height * 0.7)
+          .attr('width', width * 0.7)
+          .attr('xlink:href', imagePath);
+        imgContainer.style('height', `${height * 0.7}px`)
+          .style('width', `${width * 0.7}px`)
+          .style('margin', 'auto auto 10px auto');
+      };
 
-    img.src = imagePath;
-    img.onload = (img) => { //Images are defaulting to 0,0 on some browsers. So this is a workaround
-      let width = img.srcElement.width;
-      let height = img.srcElement.height;
-      imgElem.attr('height', height*0.7)
-      .attr('width', width*0.7)
-      .attr('xlink:href', imagePath);
-      imgContainer.style('height',height*0.7 + "px")
-      .style('width',width*0.7 + "px")
-      .style('margin', "auto auto 10px auto");
-    };
-
-      folder.files.forEach(file => {
-        d3.json('src/data/'+folder.foldername+'/'+file.filename+'.geojson', function(error, data) {
-          data.features.forEach(feature => {
-            feature.geometry.coordinates.forEach(coord => {
-              coord.forEach(subCoord => {
-                subCoord[0] *= folder.xFactor*0.7;
-                subCoord[1] *= folder.yFactor*0.7;
+      folder.files.forEach((file) => {
+        d3.json(`src/data/${folder.foldername}/${file.filename}.geojson`, (geoJSONData) => {
+          geoJSONData.features.forEach((feature) => {
+            feature.geometry.coordinates.forEach((coord) => {
+              coord.forEach((subCoord) => {
+                subCoord[0] *= folder.xFactor * 0.7;
+                subCoord[1] *= folder.yFactor * 0.7;
               });
             });
           });
-          let overlays = svg.append('g')
+          const overlays = svg.append('g')
           .attr('class', 'overlays');
 
-          let path = d3.geoPath();
+          const path = d3.geoPath();
           overlays.selectAll('path')
-          .data(data.features)
-          .enter()
-          .append('path')
-          .attr('d', path)
-          .attr('fill', file.color)
-          .attr('opacity', 0.7)
-          .attr('stroke', 'black')
-          .attr('stroke-width', '1');
+            .data(geoJSONData.features)
+            .enter()
+            .append('path')
+            .attr('d', path)
+            .attr('fill', file.color)
+            .attr('opacity', 0.7)
+            .attr('stroke', 'black')
+            .attr('stroke-width', '1');
         });
       });
     });
